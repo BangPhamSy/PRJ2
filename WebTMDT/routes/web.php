deccafdc49f3d725379b90334e6ddb5d2ff0f1e5
BangPS <bangphamsy97@gmail.com>
2017-10-19 20:41:49 +0700

    thêm giao diện trang người dùng

--- WebTMDT/routes/web.php
+++ WebTMDT/routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('index',function(){
+	return view('pages_client.trangchu');
+});
+Route::get('login',function(){
+	return view('pages_client.login');
+});
+Route::get('shop',function(){
+	return view('pages_client.shop');
+});
+Route::get('blog',function(){
+	return view('pages_client.blog');
+});
+Route::get('cart',function(){
+	return view('pages_client.cart');
+});
+Route::get('contact',function(){
+	return view('pages_client.contact');
+});
+Route::get('product_detail',function(){
+	return view('pages_client.product_detail');
+});
+Route::get('checkout',function(){
+	return view('pages_client.checkout');
+});
+Route::get('blog_single',function(){
+	return view('pages_client.blog_single');
+});
+
+
+
+
+
